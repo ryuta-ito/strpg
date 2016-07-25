d083426e969bd93b0a41d4777d6c3efba0082f33
ryuta-ito <ir47839@gmail.com>
2016-07-25 20:02:03 +0900

    yaml読み込みを改善

--- lib/dirs.rb
+++ lib/dirs.rb
@@ -1,9 +1,10 @@
+require_relative 'config.rb'
+
 module Dirs
-  WRITABLE_DIRS = Dir.glob('./test/**') + Dir.glob('./lib/**') + ['./Rakefile']
+  config = Config::YamlFile.new
+  WRITABLE_DIRS = config.all_strict_files
   ALL_TASK_DIRS = Dir.glob('./task/**/task_*')
   TEST_TASK_DIRS = Dir.glob('./task/test/**/task_*')
   SRC_TASK_DIRS = Dir.glob('./task/lib/**/task_*')
   SAPMLE_TASK_DIRS = Dir.glob('./task/sample/**')
-  class Yaml
-  end
 end
