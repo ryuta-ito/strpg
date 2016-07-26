e0c3d68f63bf02a98afc41ec279b114449030412
ryuta-ito <ir47839@gmail.com>
2016-07-26 20:36:25 +0900

    mv lib/strpg

---
+++ lib/strpg/config.rb
@@ -0,0 +1,29 @@
+require 'yaml'
+
+module Config
+  class YamlFile
+    def initialize yaml_file_path=nil
+      yaml_file_path ||= './strpg.yml'
+      begin
+        @yaml_hash = YAML.load_file yaml_file_path
+      rescue
+        @yaml_hash = {"strict_dirs" => [], "strict_files" => []}
+      end
+    end
+
+    def strict_dirs
+      @yaml_hash["strict_dirs"]
+    end
+
+    def strict_files
+      @yaml_hash["strict_files"]
+    end
+
+    def all_strict_files
+      files = (strict_dirs + strict_files).map do |file|
+        Dir[file]
+      end.reduce(&:+)
+      files.nil? ? [] : files
+    end
+  end
+end
