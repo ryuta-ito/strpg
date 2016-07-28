b42571cd4d34a17ba273715b8f8b207112dd6cf3
ryuta-ito <ir47839@gmail.com>
2016-07-28 19:35:55 +0900

    add strpg development command (hardcording)

--- bin/thor.rb
+++ bin/thor.rb
@@ -29,6 +29,15 @@ class Strpg < Thor
       f.puts "strict_files: []"
     end
   end
+
+  desc 'developmenting', 'development is starting'
+  def developmenting
+    dirs = ['./', './lib', './test', './task', './doc']
+    option =  dirs.map{|dir| File.join File.expand_path('../../', __FILE__), dir}.reduce('') do |_option, path|
+       _option + " --tab-with-profile=Default --working-directory=\"#{path}\""
+    end
+    `gnome-terminal #{option}`
+  end
 end
 
 Strpg.start(ARGV)
